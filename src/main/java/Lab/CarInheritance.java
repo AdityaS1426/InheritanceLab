b15358078237f66a789aec78aff8f7c22392e201
AdityaS1426 <71394490+AdityaS1426@users.noreply.github.com>
2021-04-18 11:24:26 -0700

    Initial commit

---
+++ src/main/java/Lab/CarInheritance.java
@@ -0,0 +1,8 @@
+package Lab;
+
+public class CarInheritance {
+    public static void main(String args[]){
+        SportsCar sc = new SportsCar(5,200,12);
+        System.out.println(sc.toString());
+    }
+}
