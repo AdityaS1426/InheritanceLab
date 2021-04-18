45504b016d02fe9eab760dd7adc622b85f10fc7a
AdityaS1426 <71394490+AdityaS1426@users.noreply.github.com>
2021-04-18 11:47:23 -0700

    Updated the controller.

--- src/main/java/com/lab/inheritance/MainController.java
+++ src/main/java/com/lab/inheritance/MainController.java
@@ -1,13 +1,23 @@
 package com.lab.inheritance;
 
+import Lab.CarInheritance;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String Index() {
+    @RequestMapping(value = "Lab/Car", method = {RequestMethod.GET, RequestMethod.POST})
+    public String Car(@RequestParam(value = "Gears", required = false, defaultValue = "3") String Gears, @RequestParam(value = "Speed", required = false, defaultValue = "3") String Speed, @RequestParam(value = "Height", required = false, defaultValue = "3") String Height, Model model) {
+
+        CarInheritance car = new CarInheritance();
+        int DaGear = Integer.parseInt(Gears);
+        int DaSpeed = Integer.parseInt(Speed);
+        int DaHeight = Integer.parseInt(Height);
+
+        model.addAttribute("paths", car);
+
         return "Index";
     }
 }
